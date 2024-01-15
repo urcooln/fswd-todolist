407938949829a120aa9ce1798307fed0e02e0c07
John Jimenez <jjimenez19961@gmail.com>
2024-01-15 17:08:16 -0500

    first commit

---
+++ app/javascript/src/index.js
@@ -0,0 +1,16 @@
+import $ from 'jquery';
+
+import {
+    indexTasks,
+    postTask,
+  } from "./requests.js";
+  
+  indexTasks(function (response) {
+    var htmlString = response.tasks.map(function(task) {
+      return "<div class='col-12 mb-3 p-2 border rounded task' data-id='" + task.id + "'> \
+        " + task.content + "\
+        </div>";
+    });
+    $("#tasks").html(htmlString);
+  });
+  
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_18_113612) do
+ActiveRecord::Schema.define(version: 2022_03_18_113614) do
 
   create_table "tasks", force: :cascade do |t|
     t.integer "user_id"
