35c4f69e859a9cdf2ccf839d0ce1d5ca18ef8d35
John Jimenez <jjimenez19961@gmail.com>
2024-01-17 17:59:29 -0500

    version 1

--- app/javascript/src/index.js
+++ app/javascript/src/index.js
@@ -1,16 +1,61 @@
 import $ from 'jquery';
 
 import {
+    deleteTask,
     indexTasks,
     postTask,
-  } from "./requests.js";
-  
-  indexTasks(function (response) {
-    var htmlString = response.tasks.map(function(task) {
-      return "<div class='col-12 mb-3 p-2 border rounded task' data-id='" + task.id + "'> \
-        " + task.content + "\
-        </div>";
+    toggleTaskStatus,
+} from "./requests.js";
+
+indexTasks(function (response) {
+    var htmlString = response.tasks.map(function (task) {
+        var taskHtml = `<div class='col-12 mb-3 p-2 border rounded task' data-id='${task.id}'>
+                            <p>${task.content}</p>
+                            <div class='form-check form-switch'>
+                                <input class='form-check-input statusCheckbox' type='checkbox' data-id='${task.id}' ${task.completed ? 'checked' : ''}>
+                                <label class='form-check-label'>Mark Complete</label>
+                            </div>
+                            <button class='btn btn-danger deleteButton' data-id='${task.id}'>Delete</button>
+                        </div>`;
+        return taskHtml;
     });
+
     $("#tasks").html(htmlString);
-  });
-  
+
+    // Add change event for status checkboxes
+    $(".statusCheckbox").change(function () {
+        var taskId = $(this).data("id");
+        var isChecked = $(this).prop("checked");
+        toggleTaskStatus(taskId, isChecked);
+    });
+
+    // Add click event for delete buttons
+    $(".deleteButton").click(function () {
+        var taskId = $(this).data("id");
+        deleteTask(taskId);
+    });
+});
+
+function handleFormSubmission(event) {
+    event.preventDefault();
+
+    var newTaskContent = $('#newTaskInput').val();
+
+    if (newTaskContent.trim() !== '') {
+        postTask(newTaskContent);
+    }
+
+    $('#newTaskInput').val('');
+}
+
+$(document).ready(function () {
+    var addTaskFormHtml = `<form id='addTaskForm' class='mb-3'>
+                                <div class='input-group'>
+                                    <input type='text' id='newTaskInput' class='form-control' placeholder='Enter a new task' required>
+                                    <button type='submit' class='btn btn-primary'>Add Task</button>
+                                </div>
+                            </form>`;
+    $("#addTaskSection").html(addTaskFormHtml);
+
+    $('#addTaskForm').submit(handleFormSubmission);
+});
--- app/javascript/src/requests.js
+++ app/javascript/src/requests.js
@@ -17,7 +17,7 @@ export var indexTasks = function (successCB, errorCB) {
   $.ajax(request);
 };
 
-export var postTask = function (content, successCB, errorCB) {
+export var postTask = function (content) {
   var request = {
     type: 'POST',
     url: 'api/tasks?api_key=1',
@@ -26,9 +26,43 @@ export var postTask = function (content, successCB, errorCB) {
         content: content
       }
     },
-    success: successCB,
-    error: errorCB
+    success: function(){
+        location.reload();
+    },
+    error: function(error){
+        console.error('Error adding task: ', error)
+    }
   }
 
   $.ajax(request);
 };
+
+export var deleteTask = function (dataId) {
+    var request = {
+        type: 'DELETE',
+        url: `api/tasks/${dataId}?api_key=1`,
+        success: function(){
+            location.reload();
+        },
+        error: function(error){
+            console.error('Error deleting task')
+        }
+    };
+    $.ajax(request);
+};
+
+export function toggleTaskStatus(taskId, isChecked) {
+    var newStatus = isChecked ? 'Complete' : 'Active';
+
+    $.ajax({
+        type: 'PUT',
+        url: 'api/tasks/' + taskId + '/' + (newStatus === 'Complete' ? 'mark_complete' : 'mark_active') + '?api_key=1',
+        success: function() {
+
+            indexTasks()
+        },
+        error: function(error) {
+            console.error('Error updating task status:', error);
+        }
+    });
+}
